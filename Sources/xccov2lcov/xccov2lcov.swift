652c0275ac0802d9bdb555a26ef5ed29f71c0895
Lukas Kollmer <hey@lukaskollmer.de>
2025-04-04 22:10:44 +0200

    add ability to pipe in the input (via reading it from stdin)

--- Sources/xccov2lcov/xccov2lcov.swift
+++ Sources/xccov2lcov/xccov2lcov.swift
@@ -17,27 +17,35 @@ extension Mode: ExpressibleByArgument {}
 @main
 struct xccov2lcov: ParsableCommand {
     @Argument(help: "Input filename (output of `xccov view --report --json file.xcresult`)")
-    var inputFilename: String
+    var inputFilename: String?
     
     @Option(help: "Path to trim from start of paths in input file")
     var trimPath: String = ""
     
-    @Option(help: "")
+    @Option(help: "Targets to include in output (default: all targets)")
     var targets: [String] = []
     
     @Option(help: "mode")
     var mode: Mode = .simple
     
     func run() throws {
-        guard FileManager().isReadableFile(atPath: inputFilename) else {
-            throw CmdError(description: "Cannot read input file: \(inputFilename)")
+        let data: Data
+        if let inputFilename {
+            guard FileManager().isReadableFile(atPath: inputFilename) else {
+                throw CmdError(description: "Cannot read input file: \(inputFilename)")
+            }
+            data = try Data(contentsOf: URL(fileURLWithPath: inputFilename))
+        } else {
+            data = sequence(first: "", next: { _ in readLine(strippingNewline: false) })
+                .reduce(into: Data()) { data, line in
+                    data.append(contentsOf: line.utf8)
+                }
         }
         let context = XCCovContext(
             includedTargets: targets,
             trimPath: trimPath,
             mode: mode
         )
-        let data = try Data(contentsOf: URL(fileURLWithPath: inputFilename))
         let xccovData = try JSONDecoder().decode(XCCovData.self, from: data)
         print(xccovData.lcov(context: context))
     }
